cc3bd348b0c7648de52aa505408a2adfa6ea3fdf
malindu21 <malindu21@gmail.com>
2025-07-11 00:07:32 +0530

    Deploy Flutter web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"dd93de6fb1776398bf586cbd477deade1391c7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3260901096"
+    serviceWorkerVersion: "2361478071"
   }
 });
